e2c2c0085b6e3a966f9847f9c7ac9e68f4e4d328
harsh2504 <harsh2504patil@gmail.com>
2024-04-08 00:52:15 +0530

    icons updated

--- admin/monthlybill/payment_history.php
+++ admin/monthlybill/payment_history.php
@@ -4,6 +4,7 @@
                                 <meta charset='utf-8'>
                                 <meta name='viewport' content='width=device-width, initial-scale=1'>
                                 <title>Payment History</title>
+                                <link rel="icon" type="image/png" href="logo.png">
                                 <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet'>
                                 <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
                                 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
